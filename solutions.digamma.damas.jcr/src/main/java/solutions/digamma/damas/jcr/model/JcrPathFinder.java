ff7754a5c079dec8c68e5c640cc3c81d4f62e162
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-07-13 19:35:37 +0200

    * Refactor session user as an interface. * Add comments skeleton implementation.

---
+++ solutions.digamma.damas.jcr/src/main/java/solutions/digamma/damas/jcr/model/JcrPathFinder.java
@@ -0,0 +1,49 @@
+package solutions.digamma.damas.jcr.model;
+
+import solutions.digamma.damas.DocumentException;
+import solutions.digamma.damas.Entity;
+import solutions.digamma.damas.PathFinder;
+import solutions.digamma.damas.auth.Token;
+import solutions.digamma.damas.inspection.NotNull;
+import solutions.digamma.damas.jcr.session.SessionUser;
+import solutions.digamma.damas.jcr.error.JcrExceptionMapper;
+import solutions.digamma.damas.jcr.session.SessionWrapper;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+/**
+ * @author Ahmad Shahwan
+ */
+public interface JcrPathFinder<T extends Entity>
+        extends SessionUser, PathFinder<T> {
+
+    @Override
+    default T find(@NotNull Token token, @NotNull  String path)
+            throws DocumentException {
+        try (SessionWrapper session = openSession(token)) {
+            if (path.startsWith("/")) {
+                path = path.substring(1);
+            }
+            if (path.equals("")) {
+                path = ".";
+            }
+            return this.find(
+                    session.getSession(), path);
+        } catch (RepositoryException e) {
+            throw JcrExceptionMapper.map(e);
+        }
+    }
+
+    /**
+     * Perform path look-up.
+     *
+     * @param session
+     * @param path
+     * @return
+     * @throws RepositoryException
+     * @throws DocumentException
+     */
+    T find(@NotNull Session session, String path)
+            throws RepositoryException, DocumentException;
+}
