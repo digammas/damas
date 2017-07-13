d1a126fc9bdb7c198a5353d228f8677c0a008957
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-07-13 19:35:37 +0200

    Refactoring and implementation
    * Refactor session user as an interface.
    * Add comments skeleton implementation.

---
+++ solutions.digamma.damas.jcr/src/main/java/solutions/digamma/damas/jcr/model/JcrSearchEngine.java
@@ -0,0 +1,81 @@
+package solutions.digamma.damas.jcr.model;
+
+import solutions.digamma.damas.DocumentException;
+import solutions.digamma.damas.Entity;
+import solutions.digamma.damas.Page;
+import solutions.digamma.damas.SearchEngine;
+import solutions.digamma.damas.auth.Token;
+import solutions.digamma.damas.inspection.NotNull;
+import solutions.digamma.damas.inspection.Nullable;
+import solutions.digamma.damas.jcr.session.SessionUser;
+import solutions.digamma.damas.jcr.error.JcrExceptionMapper;
+import solutions.digamma.damas.jcr.session.SessionWrapper;
+import solutions.digamma.damas.logging.Logged;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+/**
+ * @author Ahmad Shahwan
+ */
+public interface JcrSearchEngine<T extends Entity>
+        extends SessionUser, SearchEngine<T> {
+
+    /**
+     * Default result page size.
+     */
+    int DEFAULT_PAGE_SIZE = 30;
+
+    /**
+     * The size of returned result page when no size is specified.
+     *
+     * @return
+     */
+    default int getDefaultPageSize() {
+        return DEFAULT_PAGE_SIZE;
+    }
+
+    @Override
+    default Page<T> find(@NotNull Token token) throws DocumentException {
+        return this.find(token, 0, this.getDefaultPageSize(), null);
+    }
+
+    @Override
+    default Page<T> find(
+            @NotNull Token token, int offset, int size)
+            throws DocumentException {
+        return this.find(token, offset, size, null);
+    }
+
+    @Logged
+    @Override
+    default Page<T> find(
+            @NotNull Token token, int offset, int size, @Nullable Object query)
+            throws DocumentException {
+        try (SessionWrapper session = openSession(token)) {
+            return this.find(
+                    session.getSession(), offset, size, query);
+        } catch (RepositoryException e) {
+            throw JcrExceptionMapper.map(e);
+        }
+    }
+
+
+    /**
+     * Perform search.
+     *
+     * @param session
+     * @param offset
+     * @param size
+     * @param query
+     * @return
+     * @throws RepositoryException
+     * @throws DocumentException
+     */
+    Page<T> find(
+            @NotNull Session session,
+            int offset,
+            int size,
+            @Nullable Object query)
+            throws RepositoryException, DocumentException;
+}
---
+++ solutions.digamma.damas.jcr/src/main/java/solutions/digamma/damas/jcr/session/SessionUser.java
@@ -0,0 +1,44 @@
+package solutions.digamma.damas.jcr.session;
+
+import solutions.digamma.damas.AuthenticationException;
+import solutions.digamma.damas.DocumentException;
+import solutions.digamma.damas.auth.Token;
+import solutions.digamma.damas.inspection.NotNull;
+
+/**
+ * Session user.
+ *
+ * @author Ahmad Shahwan
+ */
+public interface SessionUser {
+
+    /**
+     * Obtain a session wrapper that contains a valid session. Returned session
+     * need not be open.
+     *
+     * @param token     Access token.
+     * @return          A valid, non-null session wrapper.
+     * @throws AuthenticationException  When no session is found for the given
+     * token.
+     */
+    @NotNull
+    SessionWrapper getSession(@NotNull Token token)
+            throws AuthenticationException;
+
+    /**
+     * Obtain a session wrapper that contains a valid and already open session.
+     *
+     * @param token     Access token.
+     * @return          A valid, non-null open session wrapper.
+     * @throws AuthenticationException  When no session is found for the given
+     * token.
+     * @throws DocumentException        When things go wrong while opening
+     * session.
+     */
+    @NotNull
+    default SessionWrapper openSession(@NotNull Token token)
+            throws DocumentException {
+        return this.getSession(token).open();
+    }
+
+}
