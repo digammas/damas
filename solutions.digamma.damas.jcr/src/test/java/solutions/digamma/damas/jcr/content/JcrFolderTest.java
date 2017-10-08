508fe277b7e324844dd6cea4af7be22312f0274c
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-10-08 09:26:13 +0200

    JCR folder tests
    * Add UTs.
    * Fix minor issue (method `getContent()` not returning null when it should).
    * Adjust comment.

--- solutions.digamma.damas.jcr/src/test/java/solutions/digamma/damas/jcr/content/JcrFolderTest.java
+++ solutions.digamma.damas.jcr/src/test/java/solutions/digamma/damas/jcr/content/JcrFolderTest.java
@@ -4,32 +4,39 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import solutions.digamma.damas.auth.Token;
+import solutions.digamma.damas.content.DetailedFolder;
 import solutions.digamma.damas.content.Folder;
 import solutions.digamma.damas.content.FolderManager;
+import solutions.digamma.damas.inspection.NotNull;
 import solutions.digamma.damas.jcr.Mocks;
 import solutions.digamma.damas.jcr.WeldTest;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
- * Created by ahmad on 9/3/17.
+ * Test folder object.
+ *
+ * Created by Ahmad on 9/3/17.
  */
 public class JcrFolderTest extends WeldTest {
 
-    private FolderManager manager;
     private Token token;
     private Folder folder;
 
     @Before
     public void setUp() throws Exception {
-        this.manager = inject(JcrFolderManager.class);
+        FolderManager manager = inject(JcrFolderManager.class);
         this.token = this.login.login("admin", "admin");
-        String parentId = this.manager
+        String parentId = manager
                 .find(this.token).getObjects().iterator().next().getId();
         Folder parent = this.folder = manager.create(this.token,
                 Mocks.folder(parentId, "test"));
         for (int i = 0; i < 10; i++) {
             parentId = parent.getId();
-            parent = this.folder = manager.create(this.token,
-                    Mocks.folder(parentId, "test"));
+            String name = String.format("test_%d", i);
+            parent = manager.create(this.token,
+                    Mocks.folder(parentId, name));
         }
 
     }
@@ -41,18 +48,36 @@ public class JcrFolderTest extends WeldTest {
 
     @Test
     public void expand() throws Exception {
+        this.folder.expand();
+
     }
 
     @Test
     public void expandContent() throws Exception {
+        Folder expanded = this.folder;
+        expanded.expandContent();
+        for (int i = 0; i < 10; i++) {
+            expanded = expanded.getContent().getFolders().iterator().next();
+        }
     }
 
     @Test
-    public void expandContent1() throws Exception {
+    public void expandContentToDepth() throws Exception {
+        Folder expanded = this.folder;
+        int depth = 5;
+        expanded.expandContent(depth);
+        for (int i = 0; i < depth; i++) {
+            expanded = expanded.getContent().getFolders().iterator().next();
+        }
+        assert expanded.getContent() == null;
     }
 
     @Test
     public void getContent() throws Exception {
+        this.folder.expandContent();
+        List<? extends Folder> folders = this.folder.getContent().getFolders();
+        assert folders.size() != 0;
+        Folder subfolder = folders.iterator().next();
+        assert subfolder.getName().equals(String.format("test_%d", 0));
     }
-
 }
