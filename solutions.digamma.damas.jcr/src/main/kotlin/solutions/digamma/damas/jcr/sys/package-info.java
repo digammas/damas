775d219db63ab26261616d5ceec1309213c5fc6c
Ahmad Shahwan <ahmad.shahwan@april.com>
2017-11-10 12:23:16 +0100

    System users
    * Add package-level comments.

---
+++ solutions.digamma.damas.jcr/src/main/kotlin/solutions/digamma/damas/jcr/sys/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package provides access to system users.
+ * </p>
+ * System users are user-sessions used by the DMS system to achieve anonymous
+ * tasks, such as repository booting.
+ * </p>
+ * Two system users are available, a superuser with read-write privileges, and
+ * a readonly user with read-only privileges. System users have access to the
+ * whole repository.
+ */
+package solutions.digamma.damas.jcr.sys;
