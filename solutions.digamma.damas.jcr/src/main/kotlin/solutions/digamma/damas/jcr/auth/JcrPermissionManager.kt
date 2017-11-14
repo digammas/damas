85e42f74300a7213504801434ed510d7a633f7a7
Ahmad Shahwan <ahmad.shahwan@april.com>
2017-11-14 17:00:02 +0100

    More untested implementation
    * JCR permission manager.

--- solutions.digamma.damas.jcr/src/main/kotlin/solutions/digamma/damas/jcr/auth/JcrPermissionManager.kt
+++ solutions.digamma.damas.jcr/src/main/kotlin/solutions/digamma/damas/jcr/auth/JcrPermissionManager.kt
@@ -6,7 +6,6 @@ import solutions.digamma.damas.common.WorkspaceException
 import solutions.digamma.damas.jcr.common.Exceptions
 import solutions.digamma.damas.jcr.model.JcrCrudManager
 import solutions.digamma.damas.login.Token
-import javax.jcr.RepositoryException
 import javax.jcr.Session
 
 /**
@@ -15,22 +14,40 @@ import javax.jcr.Session
 internal class JcrPermissionManager:
         JcrCrudManager<Permission>(), PermissionManager {
 
-    @Throws(WorkspaceException::class, RepositoryException::class)
+    @Throws(WorkspaceException::class)
+    override fun retrieve(token: Token, fileId: String, subjectId: String):
+            Permission {
+        return Exceptions.wrap(openSession(token)) {
+            retrieve(it.getSession(), fileId, subjectId)
+        }
+    }
+
+    @Throws(WorkspaceException::class)
+    override fun retrieveAt(token: Token, fileId: String): List<Permission> {
+        return Exceptions.wrap(openSession(token)) {
+            retrieveAt(it.getSession(), fileId)
+        }
+    }
+
+    @Throws(WorkspaceException::class)
+    override fun updateAt(token: Token, fileId: String, permissions: List<Permission>) {
+        return Exceptions.wrap(openSession(token)) {
+            updateAt(it.getSession(), fileId, permissions)
+        }
+    }
+
     override fun update(session: Session, id: String, pattern: Permission) =
             this.retrieve(session, id).also {
         it.accessRights = pattern.accessRights
     }
 
-    @Throws(WorkspaceException::class)
     override fun delete(session: Session, id: String) {
         this.retrieve(session, id).remove()
     }
 
-    @Throws(WorkspaceException::class)
     override fun retrieve(session: Session, id: String) =
         JcrPermission.of(session, id)
 
-    @Throws(WorkspaceException::class)
     override fun create(session: Session, pattern: Permission): Permission {
         return JcrPermission
                 .of(session, pattern.objectId, pattern.subjectId).also {
@@ -38,17 +55,15 @@ internal class JcrPermissionManager:
         }
     }
 
-    @Throws(WorkspaceException::class)
-    override fun retrieve(token: Token, fileId: String, subjectId: String):
-            Permission {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    fun retrieve(session: Session, fileId: String, subjectId: String) =
+            JcrPermission.of(session, fileId, subjectId)
 
-    override fun retrieveAt(token: Token?, fileId: String?): MutableList<Permission> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    fun retrieveAt(session: Session, fileId: String) =
+        JcrPermission.lisOf(session, fileId)
 
-    override fun updateAt(token: Token?, fileId: String?, permissions: MutableList<Permission>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun updateAt(session: Session, fileId: String, permissions: List<Permission>) {
+        permissions.reversed().forEach {
+            update(session, "$fileId${JcrPermission.ID_SEPARATOR}${it.subjectId}", it)
+        }
     }
 }
