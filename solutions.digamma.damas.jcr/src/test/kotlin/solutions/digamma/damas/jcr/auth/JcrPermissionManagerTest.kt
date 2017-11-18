88754c821ea78ae4ad1a52d1b268c3335f29315f
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-11-18 10:31:42 +0100

    Unit tests for JCR authorization implementation

--- solutions.digamma.damas.jcr/src/test/kotlin/solutions/digamma/damas/jcr/auth/JcrPermissionManagerTest.kt
+++ solutions.digamma.damas.jcr/src/test/kotlin/solutions/digamma/damas/jcr/auth/JcrPermissionManagerTest.kt
@@ -1,10 +1,40 @@
 package solutions.digamma.damas.jcr.auth
 
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
+import solutions.digamma.damas.jcr.WeldTest
+import solutions.digamma.damas.login.Token
+import solutions.digamma.damas.user.User
+import solutions.digamma.damas.user.UserManager
 
-import org.junit.Assert.*
+class JcrPermissionManagerTest  : WeldTest() {
 
-class JcrPermissionManagerTest {
+    private val userManager = WeldTest.inject(UserManager::class.java)
+    private lateinit var token: Token
+    private lateinit var username: String
+    private lateinit var userToken: Token
+
+    @Before
+    fun setUp() {
+        this.token = this.login.login("admin", "admin")
+        val user = Mockito.mock(User::class.java)
+        Mockito.`when`(user.login).thenReturn("tester")
+        this.username = this.userManager.create(this.token, user).login
+        val password = "P@55w0rd"
+        this.userManager.updatePassword(this.token, this.username, password)
+        this.userToken = this.login.login(this.username, password)
+    }
+
+    @After
+    fun tearDown() {
+        this.login.logout(this.userToken)
+        this.userManager.delete(this.token, this.username)
+        this.login.logout(this.token)
+    }
+
+
     @Test
     fun retrieve() {
     }
@@ -23,10 +53,6 @@ class JcrPermissionManagerTest {
 
     @Test
     fun delete() {
-    }
-
-    @Test
-    fun retrieve1() {
     }
 
     @Test
