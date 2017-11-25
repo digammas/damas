0b67fb9b880569a74eccb0d9609d4ab1bce49a5d
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-11-25 12:00:51 +0100

    Minimize SystemRole

--- solutions.digamma.damas.jcr/src/main/kotlin/solutions/digamma/damas/jcr/sys/SystemRole.kt
+++ solutions.digamma.damas.jcr/src/main/kotlin/solutions/digamma/damas/jcr/sys/SystemRole.kt
@@ -1,21 +1,15 @@
 package solutions.digamma.damas.jcr.sys
 
 import java.security.Principal
-import java.security.acl.Group
-import java.util.Enumeration
-import java.util.Vector
 
 /**
- * System principals. Those are principals that can run a JCR repository as
- * expected by modeshape.
+ * System principals. Those are principals that can run a JCR repository.
  */
 enum class SystemRole(private val principal: String): Principal {
 
     READONLY("readonly"),
     READWRITE("readwrite"),
     ADMIN("admin");
-
-    private val vector = Vector<Principal>(1)
 
     override fun getName(): String = this.principal
 }
