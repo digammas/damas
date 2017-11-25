f04871593543712ca803ab1b23627249a8acf353
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-11-25 11:53:03 +0100

    Add utility  and  methods to

--- solutions.digamma.damas.api/src/main/java/solutions/digamma/damas/auth/AccessRight.java
+++ solutions.digamma.damas.api/src/main/java/solutions/digamma/damas/auth/AccessRight.java
@@ -1,5 +1,7 @@
 package solutions.digamma.damas.auth;
 
+import java.util.EnumSet;
+
 /**
  * Access privilege.
  * An access privilege can be granted to a given subject, on given objects.
@@ -11,4 +13,22 @@ public enum AccessRight {
     READ,
     WRITE,
     SHARE;
+
+    /**
+     * None of these.
+     *
+     * @return empty enum set
+     */
+    public static EnumSet<AccessRight> none() {
+        return EnumSet.noneOf(AccessRight.class);
+    }
+
+    /**
+     * All of these.
+     *
+     * @return all access rights
+     */
+    public static EnumSet<AccessRight> all() {
+        return EnumSet.allOf(AccessRight.class);
+    }
 }
