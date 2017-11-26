33b32ad54f26bb1a7253fb43ddb98b45ccccd7d8
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-11-26 17:51:35 +0100

    Extract a permission-protected exception

---
+++ solutions.digamma.damas.jcr/src/main/kotlin/solutions/digamma/damas/jcr/auth/PermissionProtectedException.kt
@@ -0,0 +1,9 @@
+package solutions.digamma.damas.jcr.auth
+
+import solutions.digamma.damas.common.ConflictException
+
+/**
+ * Exception thrown upon attempt to modify protected permission.
+ */
+internal class PermissionProtectedException(subject: String, path: String):
+        ConflictException("Permission for $subject at $path is protected.")
