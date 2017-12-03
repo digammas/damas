d5af8bd1fc0059ec728ddf44513896daf9e90a87
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2017-12-03 16:18:33 +0100

    Add test for membership update

--- solutions.digamma.damas.jcr/src/test/kotlin/solutions/digamma/damas/jcr/user/JcrUserManagerTest.kt
+++ solutions.digamma.damas.jcr/src/test/kotlin/solutions/digamma/damas/jcr/user/JcrUserManagerTest.kt
@@ -8,7 +8,10 @@ import solutions.digamma.damas.login.Token
 import solutions.digamma.damas.common.InvalidArgumentException
 import solutions.digamma.damas.common.NotFoundException
 import solutions.digamma.damas.jcr.WeldTest
+import solutions.digamma.damas.user.Group
+import solutions.digamma.damas.user.GroupManager
 import solutions.digamma.damas.user.User
+import java.util.Arrays
 
 class JcrUserManagerTest: WeldTest() {
 
@@ -53,6 +56,58 @@ class JcrUserManagerTest: WeldTest() {
         assert(user.firstName == entity.firstName)
         assert(user.lastName == entity.lastName)
         this.manager.delete(this.token, id)
+    }
+
+    @Test
+    fun updateGroups() {
+        /* Set up groups */
+        val gm = WeldTest.inject(GroupManager::class.java)
+        val group = Mockito.mock(Group::class.java)
+        val testers = "testers"
+        val devops = "devops"
+        Mockito.`when`(group.name).thenReturn(testers)
+        gm.create(this.token, group)
+        Mockito.`when`(group.name).thenReturn(devops)
+        gm.create(this.token, group)
+
+        /* Set up user */
+        var user = Mockito.mock(User::class.java)
+        Mockito.`when`(user.login).thenReturn("tester")
+        val id = this.manager.create(this.token, user).id
+
+        /* Add */
+        user = Mockito.mock(User::class.java)
+        Mockito.`when`(user.memberships).thenReturn(
+                Arrays.asList(testers, devops)
+        )
+        this.manager.update(this.token, id, user)
+        assert(this.manager.retrieve(this.token, id)
+                .memberships == user.memberships)
+
+        /* Remove */
+        user = Mockito.mock(User::class.java)
+        Mockito.`when`(user.memberships).thenReturn(
+                Arrays.asList(testers)
+        )
+        this.manager.update(this.token, id, user)
+        assert(this.manager.retrieve(this.token, id)
+                .memberships == user.memberships)
+
+        /* Add back */
+        user = Mockito.mock(User::class.java)
+        Mockito.`when`(user.memberships).thenReturn(
+                Arrays.asList(testers, devops)
+        )
+        this.manager.update(this.token, id, user)
+        assert(this.manager.retrieve(this.token, id)
+                .memberships == user.memberships)
+
+        /* Tear down user */
+        this.manager.delete(this.token, id)
+
+        /* Tear down groups */
+        gm.delete(this.token, testers)
+        gm.delete(this.token, devops)
     }
 
     @Test
