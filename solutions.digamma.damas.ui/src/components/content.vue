2cb7b0d2c82e28a78e94301818a460c47feedb8b
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-04-16 18:39:12 +0200

    Add link to parent folder

--- solutions.digamma.damas.ui/src/components/content.vue
+++ solutions.digamma.damas.ui/src/components/content.vue
@@ -3,11 +3,17 @@
         <h1>Content Home</h1>
         <div>Current folder ID {{id}}</div>
         <div>Current folder Path {{folder.path}}</div>
-        <div v-if="folder.content.folders.length != 0">
+        <div v-if="folder.parentId" key="has-parent">
+            <router-link :to="folder.parentId">Parent Directory</router-link>
+        </div>
+        <div v-if="folder.content.folders.length != 0" key="has-subfolders">
             <h2>List of subfolders</h2>
-            <ul v-for="subfolder in folder.content.folders">
+            <ul v-for="subfolder in folder.content.folders" :key="subfolder.id">
                 <li><router-link :to="subfolder.id">{{subfolder.name}}</router-link></li>
             </ul>
+        </div>
+        <div v-else key="has-subfolders">
+            <span>No subfolfers in this directory</span>
         </div>
     </div>
 </template>
@@ -22,6 +28,8 @@ export default {
             id: null,
             folder: null
         }
+    },
+    computed: {
     },
     components: {
     },
