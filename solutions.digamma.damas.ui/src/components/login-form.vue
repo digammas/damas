139e2c40d4f653986d974cd6009faa9347d0d0a7
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-04-21 09:35:05 +0200

    Add flash message widget
    * Use new component as login error message.

--- solutions.digamma.damas.ui/src/components/login-form.vue
+++ solutions.digamma.damas.ui/src/components/login-form.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <h2>User Login</h2>
-        <div v-if="error">
-            <p>{{error}}</p>
-        </div>
+        <app-flash-message ref="errorMessage" />
         <div>
             <app-row align="center">
                 <app-text-input
@@ -36,10 +34,11 @@ import AppTextInput from "./widgets/app-text-input";
 import AppButton from "./widgets/app-button"
 import AppRow from "./widgets/app-row";
 import AppCheckbox from "./widgets/app-checkbox";
+import AppFlashMessage from "./widgets/app-flash-message";
 
 export default {
     name: "LoginForm",
-    components: {AppCheckbox, AppRow, AppButton, AppTextInput},
+    components: {AppFlashMessage, AppCheckbox, AppRow, AppButton, AppTextInput},
     data() {
         return {
             username: "",
@@ -63,6 +62,13 @@ export default {
             }).catch(reason => {
                 this.error = reason.message
             })
+        }
+    },
+    watch :{
+        error() {
+            if (this.error) {
+                this.$refs.errorMessage.show(this.error)
+            }
         }
     }
 }
---
+++ solutions.digamma.damas.ui/src/components/widgets/app-flash-message.vue
@@ -0,0 +1,30 @@
+<template>
+    <div ref="container" class="mdl-js-snackbar mdl-snackbar">
+        <div class="mdl-snackbar__text"></div>
+        <button class="mdl-snackbar__action" type="button"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppFlashMessage",
+    data() {
+        return {
+            message,
+            timeout: 2750
+        }
+    },
+    methods: {
+        show(message, timeout) {
+            this.$refs.container.MaterialSnackbar.showSnackbar({
+                message: message || this.message,
+                timeout: timeout || this.timeout
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
