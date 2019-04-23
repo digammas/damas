bf8196c483978796d0554b91848512456aa48a5d
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-04-23 10:55:43 +0200

    Install and use material icons

--- solutions.digamma.damas.ui/src/main.js
+++ solutions.digamma.damas.ui/src/main.js
@@ -5,6 +5,7 @@ import store from '@/store'
 
 import 'material-design-lite/material.min.css'
 import 'material-design-lite/material.min'
+import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
