8e1c19b5a9d558894f028b9d7f13e53a9c9f463f
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-05-11 16:42:36 +0200

    Parametrize base URL for content service

--- solutions.digamma.damas.ui/src/service/document.js
+++ solutions.digamma.damas.ui/src/service/document.js
@@ -1,37 +1,39 @@
 import http from '@/service/http'
 
+const BASE_URL = "/documents"
+
 class DocumentService {
 
     async retrieve(id, full = false) {
-        return (await http.get(`/documents/${id}`, { full })).data
+        return (await http.get(`${BASE_URL}/${id}`, { full })).data
     }
 
     async create(parentId, name, mimeType) {
-        return (await http.post("/documents", { parentId, name, mimeType })).data
+        return (await http.post(`${BASE_URL}`, { parentId, name, mimeType })).data
     }
 
     async upload(id, binary) {
-        await http.put(`/documents/${id}/upload`, binary, {}, 'blob')
+        await http.put(`${BASE_URL}/${id}/upload`, binary, {}, 'blob')
     }
 
     async download(id) {
-        return (await http.get(`/documents/${id}/download`, {}, 'blob')).data
+        return (await http.get(`${BASE_URL}/${id}/download`, {}, 'blob')).data
     }
 
     async rename(id, name) {
-        return (await http.put(`/documents/${id}`, { name })).data
+        return (await http.put(`${BASE_URL}/${id}`, { name })).data
     }
 
     async remove(id, name) {
-        return (await http.delete(`/documents/${id}`, { name })).data
+        return (await http.delete(`${BASE_URL}/${id}`, { name })).data
     }
 
     async move(id, parentId) {
-        return (await http.put(`/documents/${id}`, { parentId })).data
+        return (await http.put(`${BASE_URL}/${id}`, { parentId })).data
     }
 
     async copy(id, parentId) {
-        return (await http.post(`/documents/${id}/copy`, { parentId })).data
+        return (await http.post(`${BASE_URL}/${id}/copy`, { parentId })).data
     }
 }
 
--- solutions.digamma.damas.ui/src/service/folder.js
+++ solutions.digamma.damas.ui/src/service/folder.js
@@ -1,33 +1,35 @@
 import http from '@/service/http'
 
+const BASE_URL = "/folders"
+
 class ContentService {
 
     async retrieve(id, depth = null, full = false) {
-        return (await http.get(`/folders/${id}`, { full, depth })).data
+        return (await http.get(`${BASE_URL}/${id}`, { full, depth })).data
     }
 
     async retrieveAt(path) {
-        return (await http.get(`/folders/at${path}`)).data
+        return (await http.get(`${BASE_URL}/at${path}`)).data
     }
 
     async create(parentId, name) {
-        return (await http.post("/folders", { parentId, name })).data
+        return (await http.post(`${BASE_URL}`, { parentId, name })).data
     }
 
     async rename(id, name) {
-        return (await http.put(`/folders/${id}`, { name })).data
+        return (await http.put(`${BASE_URL}/${id}`, { name })).data
     }
 
     async remove(id, name) {
-        return (await http.delete(`/folders/${id}`, { name })).data
+        return (await http.delete(`${BASE_URL}/${id}`, { name })).data
     }
 
     async move(id, parentId) {
-        return (await http.put(`/folders/${id}`, { parentId })).data
+        return (await http.put(`${BASE_URL}/${id}`, { parentId })).data
     }
 
     async copy(id, parentId) {
-        return (await http.post(`/folders/${id}/copy`, { parentId })).data
+        return (await http.post(`${BASE_URL}/${id}/copy`, { parentId })).data
     }
 }
 
