08a82527f7b3c96009c2571ff804f961d0ea4806
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-05-11 17:01:44 +0200

    Polish user service

--- solutions.digamma.damas.ui/src/service/user.js
+++ solutions.digamma.damas.ui/src/service/user.js
@@ -1,28 +1,27 @@
 import http from '@/service/http'
 import store from '@/store'
 
+const BASE_URL = "/users"
+
 class UserService {
 
-
     async load() {
-        if (!store.state.user.valid) {
-            await this.retrieve(store.state.auth.username)
+        if (!store.state.user.valid && store.state.auth.username) {
+            let user = {
+                username: store.state.auth.username
+            }
+            try {
+                user = await this.retrieve(user.username)
+                user.fetched = true
+            } catch (e) {
+                if (e.statusCode !== 404) throw e
+            }
+            await store.dispatch("user/update", {...user, valid: true})
         }
     }
 
     async retrieve(username) {
-        try {
-            let response = await http.get(`/users/${username}`)
-            await store.dispatch("user/update", {...response.data, valid: true})
-            return response.data
-        } catch (e) {
-            if (e.statusCode === 404) {
-                store.dispatch("user/clear")
-                return null
-            } else {
-                throw e
-            }
-        }
+        return (await http.get(`${BASE_URL}/${username}`)).data
     }
 }
 
