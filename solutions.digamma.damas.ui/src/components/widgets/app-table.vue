54d7029d0d9d2e0549c55a3438d931661539a585
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-05-14 19:45:38 +0200

    Table widget

---
+++ solutions.digamma.damas.ui/src/components/widgets/app-table.vue
@@ -0,0 +1,36 @@
+<template>
+    <table
+            class="mdl-data-table mdl-js-data-table"
+            :class="classes"
+            ref="table">
+        <slot></slot>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "AppTable",
+    props: {
+        selectable: Boolean,
+        shadow: Boolean
+    },
+    computed: {
+        classes() {
+            return {
+                'mdl-shadow--2dp': this.shadow
+            }
+        }
+    },
+    mounted() {
+        componentHandler.upgradeElement(this.$refs.table)
+    }
+}
+</script>
+
+<style scoped>
+
+.mdl-data-table td {
+    text-align: inherit;
+}
+
+</style>
