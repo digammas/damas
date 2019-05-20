08afc106f24ed4d2230a8acc6c32a80419888513
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-05-20 20:14:15 +0200

    Enhance cell component

--- solutions.digamma.damas.ui/src/components/widgets/app-cell.vue
+++ solutions.digamma.damas.ui/src/components/widgets/app-cell.vue
@@ -19,34 +19,42 @@ export default {
     props: {
         span: {
             type: Number,
-            validator: validate.bind(12),
+            validator: v => Array.from({length: 12}, (_, i) => i + 1).includes(v),
             default: 4
         },
         desktopSpan: {
             type: Number,
-            validator: validate.bind(12)
+            validator: v => Array.from({length: 12}, (_, i) => i + 1).includes(v)
         },
         tabletSpan: {
             type: Number,
-            validator: validate.bind(8)
+            validator: v => Array.from({length: 8}, (_, i) => i + 1).includes(v)
         },
         mobileSpan: {
             type: Number,
-            validator: validate.bind(6)
+            validator: v => Array.from({length: 6}, (_, i) => i + 1).includes(v)
         },
         tag: {
             type: String,
             default: "div"
         },
+        align: {
+            type: String,
+            validator(value) {
+                return ["middle", "top", "bottom"].includes(value)
+            }
+        },
         hint: String
     },
     computed: {
         classes() {
-            let classes = [`mdl-cell--${this.span}-col`]
-            addClassForDevice(classes, this.desktopSpan, "desktop")
-            addClassForDevice(classes, this.tabletSpan, "tablet")
-            addClassForDevice(classes, this.mobileSpan, "mobile")
-            return classes
+            return {
+                [`mdl-cell--${this.align}`]: this.align,
+                [`mdl-cell--${this.span}-col`]: this.span,
+                [`mdl-cell--${this.desktopSpan}-col`]: this.desktopSpan,
+                [`mdl-cell--${this.tabletSpan}-col`]: this.tabletSpan,
+                [`mdl-cell--${this.mobileSpan}-col`]: this.mobileSpan
+            }
         },
         hintedId() {
             return this.$utils.randomId("inner-cell-")
@@ -59,15 +67,6 @@ export default {
     }
 }
 
-function addClassForDevice(list, span, device) {
-    if (span) {
-        list.push(`mdl-cell--${span}-col-${device}`)
-    }
-}
-
-function validate(value) {
-    return value > 0 && value <= this
-}
 </script>
 
 <style scoped>
