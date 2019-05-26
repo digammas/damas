85d746bec2fd9ed16427ee2eb8d48784ff378b99
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-05-26 17:15:08 +0200

    Add hint widget

---
+++ solutions.digamma.damas.ui/src/components/widgets/app-hint.vue
@@ -0,0 +1,51 @@
+<template>
+    <component :is="outerTag">
+        <component :id="contentId" :is="innerTag">
+            <slot></slot>
+        </component>
+        <div
+                ref="hint"
+                class="mdl-tooltip hint-box"
+                :for="contentId">
+            {{text}}
+        </div>
+    </component>
+</template>
+
+<script>
+export default {
+    name: "AppHint",
+    props: {
+        outerTag: {
+            type: String,
+            default: "span"
+        },
+        innerTag: {
+            type: String,
+            default: "span"
+        },
+        text: String
+    },
+    computed: {
+        contentId() {
+            return this.$utils.randomId("hint-content-")
+        }
+    },
+    mounted() {
+        window.componentHandler.upgradeElement(this.$refs.hint)
+    }
+}
+</script>
+
+<style scoped>
+
+.hint-box {
+    max-width: inherit;
+}
+
+.mdl-tooltip {
+    font-size: 14px;
+    padding: 6px;
+}
+
+</style>
